5f6d6855206d91b920b667555f06b1a6d8450593
divyang2710 <parmardivyang159@icloud.com>
2026-04-23 16:59:49 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'core/routes/app_router.dart';
 import 'core/theme/app_text_styles.dart';
 import 'features/watchlist/bloc/watchlist_bloc.dart';
 
+
 void main() {
   runApp(const TradePracticalApp());
 }
